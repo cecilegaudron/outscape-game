0d082ebd8f4c4319672ef58f4560003533b6ae1e
cecilegaudron <ce.gaudron@gmail.com>
2023-03-02 16:21:43 +0000

    Add list with timeslots to booking model

--- outscapebooking/views.py
+++ outscapebooking/views.py
@@ -7,7 +7,7 @@ from .models import Booking
 class BookingList(generic.ListView):
     model = Booking
     # filter the bookings with confirmed status and order by date
-    queryset = Boooking.objects.filter(status=1).order_by('-date')
+    queryset = Boooking.objects.filter(status=1).order_by('date')
     # the list view is display on the index page
     template_name = 'index.html'
     # If more of 6 bookings a new page is created
