5694d4e4c1d9ec677fb7b3af7a2e4f3bb6b0f33e
cecilegaudron <ce.gaudron@gmail.com>
2023-03-05 10:52:01 +0000

    Admin panel customization

--- outscapebooking/admin.py
+++ outscapebooking/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 from .models import Booking
 
 
@@ -7,3 +8,7 @@ class BookingAdmin(admin.ModelAdmin):
 
     list_display = ('date', 'timeslot', 'players', 'tickets', 'status', 'comment')
     list_filter = ('status', 'date')
+
+# Remove the default groups
+# https://www.section.io/engineering-education/customizing-django-admin/
+admin.site.unregister(Group)
