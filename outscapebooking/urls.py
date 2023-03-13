359a8bd22b9faab7b68a95d468db6e6eeda6bd08
cecilegaudron <ce.gaudron@gmail.com>
2023-03-13 09:20:41 +0000

    Display only future bookings

--- outscapebooking/urls.py
+++ outscapebooking/urls.py
@@ -1,10 +1,10 @@
 from . import views
 from django.urls import path
-from .views import BookingList, BookingView
+from .views import BookingList
 
 
 
 urlpatterns = [
     path('', views.BookingList.as_view(), name='index'),
-    path('', BookingView.as_view(), name='index'), 
+    #path('', BookingView.as_view(), name='index'), 
 ]
--- outscapebooking/views.py
+++ outscapebooking/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.views import generic
 from django.views.generic import TemplateView
 from .models import Booking
+from django.utils.timezone import now
+from datetime import date
 
 
 # Bookings list 
 class BookingList(generic.ListView):
     model = Booking
-    # filter the bookings with confirmed status and order by date
-    queryset = Booking.objects.filter(status=1).order_by('bookdate')
+    '''
+    Filter the bookings with confirmed status
+    Display only futur bookings
+    https://stackoverflow.com/questions/4668619/how-do-i-filter-query-objects-by-date-range-in-django/4668703#4668703
+    Order by date
+    '''
+    queryset = Booking.objects.filter(status=1, bookdate__gte=now().date()).order_by('bookdate')
     # the list view is display on the index page
     template_name = 'index.html'
     # If more of 6 bookings a new page is created
-    paginate_by = 6
-
-
-# New Try for Views
-class BookingView(TemplateView):
-    template_name = 'index.html'
+    paginate_by = 10
